b84003ff32a1914d95f6a5bfcfb1b934d733c35c
liuyuan <liuyuan03@sunlands.com>
2018-11-16 20:02:34 +0800

    Initial commit

---
+++ src/main/java/com/ly/crypt/CryptApplication.java
@@ -0,0 +1,12 @@
+package com.ly.crypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptApplication.class, args);
+    }
+}
